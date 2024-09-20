3b69690798d6c830db473826d8eea89c6148feab
Nicholas Oliveira <nich27oli@gmail.com>
2024-09-19 22:42:22 -0300

    Incluindo projeto

---
+++ NiscoGPT/Program.cs
@@ -0,0 +1,33 @@
+﻿using OpenAI_API;
+
+class Program
+{
+	static async Task Main(String[] args)
+	{
+		while (true)
+		{
+			string openAIKey = "";
+
+			var authentication = new APIAuthentication(openAIKey);
+
+			var api = new OpenAIAPI(authentication);
+
+			var chat = api.Chat.CreateConversation();
+
+			Console.WriteLine("Mensagem: ");
+			string? question = Console.ReadLine();
+
+			Console.WriteLine("*****************************************\n");
+			
+			chat.AppendUserInput(question);
+
+			Console.ForegroundColor = ConsoleColor.Green;
+			string answer = await chat.GetResponseFromChatbotAsync();
+			Console.WriteLine(answer + "\n");
+
+			Console.WriteLine("*****************************************\n");
+			Console.ResetColor();
+
+		}
+	}
+}
